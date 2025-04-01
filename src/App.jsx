fc43faa117242b6bd32ed07d3316377d94e99f6d
DEnir7760 <danielbednar09@gmail.com>
2025-04-01 22:12:26 +0200

    pohjklhjkhjkl

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,16 @@ let x =2;
 let y =3;
 
 //tabliczka mnozenia
-let a1 = n1;
-let a2 = n2;
-let a3 = n3;
-let a4 = n4;
-let a5 = n5;
-let a6 = n6;
-let a7 = n7;
-let a8 = n8;
-let a9 = n9;
-let a10 = n10;
+let a1 = {n}*1;
+let a2 = {n}*2;
+let a3 = {n}*3;
+let a4 = {n}*4;
+let a5 = {n}*5;
+let a6 = {n}*6;
+let a7 = {n}*7;
+let a8 = {n}*8;
+let a9 = {n}*9;
+let a10 = {n}*10;
   return (
     <>
      <div class="head"> <a class="nrkomp">Komputer przy którym siedzę ma numer {n}</a></div>
