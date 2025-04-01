66eaa8c445857a7a4a39799cc6329eb291a28ea1
DEnir7760 <danielbednar09@gmail.com>
2025-04-01 22:13:40 +0200

    wwweweqewqewq

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,10 @@
 import './App.css'
 
 function App() {
-  let n = 5;
-console.log(n);
-
-let x =2;
-let y =3;
-
-//tabliczka mnozenia
-let a1 = {n}*1;
-let a2 = {n}*2;
-let a3 = {n}*3;
-let a4 = {n}*4;
-let a5 = {n}*5;
-let a6 = {n}*6;
-let a7 = {n}*7;
-let a8 = {n}*8;
-let a9 = {n}*9;
-let a10 = {n}*10;
-//d/d/dd/d/d/d/d/d//d/d
+ 
   return (
     <>
-     <div class="head"> <a class="nrkomp">Komputer przy którym siedzę ma numer {n}</a></div>
+     fgcvbhnj
     </>
   )
 }
