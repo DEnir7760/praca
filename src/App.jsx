742085dc486da805f8c285bdd579bc7d69643f08
DEnir7760 <danielbednar09@gmail.com>
2025-04-01 22:08:29 +0200

    poczatek

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,26 @@
 import './App.css'
 
 function App() {
-  
+  let n = 5;
+console.log(n);
 
+let x =2;
+let y =3;
+
+//tabliczka mnozenia
+let a1 = n1;
+let a2 = n2;
+let a3 = n3;
+let a4 = n4;
+let a5 = n5;
+let a6 = n6;
+let a7 = n7;
+let a8 = n8;
+let a9 = n9;
+let a10 = n10;
   return (
     <>
-     
+     <div class="head"> <a class="nrkomp">Komputer przy którym siedzę ma numer {n}</a></div>
     </>
   )
 }
